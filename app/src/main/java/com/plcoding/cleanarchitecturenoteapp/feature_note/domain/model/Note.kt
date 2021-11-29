bf5d5afc076159bed551ac31a0f689a3040f4d21
Nicholas <niauyeung@gmail.com>
2021-11-29 12:06:16 -0800

    added and finished view model

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
@@ -16,5 +16,6 @@ data class Note (
     companion object{
         val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
     }
-    
 }
+
+class InvalidNoteException(message: String): Exception(message)
